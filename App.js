cbe6d1c92f18964e7b14fd1f1781ba6f1ad3ac6d
srinivasthiruvattur97 <srinivasthiruvattur97@gmail.com>
2025-09-04 20:40:17 +0530

    second commit

--- App.js
+++ App.js
@@ -1,6 +1,7 @@
+import React from "react";
+import ReactDOM from "react-dom/client";
 const parentdiv = React.createElement("div", { id: "parent" }, [
   React.createElement("h1", {}, "Iam h1 tag"),
-  React.createElement("h3", {}, "Iam h3 tag"),
 ]);
 console.log(parentdiv);
 const root = ReactDOM.createRoot(document.getElementById("root"));
