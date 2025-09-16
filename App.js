87a615927e00f2e5c1678b30a2d4469aec44f094
srinivasthiruvattur97 <srinivasthiruvattur97@gmail.com>
2025-09-16 13:59:12 +0530

    Added lazy loading for the Grocery component

--- App.js
+++ App.js
@@ -1,3 +1,4 @@
+import React, { lazy, Suspense } from "react";
 import ReactDOM from "react-dom/client";
 import Header from "./components/Header";
 import Body from "./components/Body";
@@ -6,6 +7,9 @@ import About from "./components/About";
 import Contact from "./components/Contact";
 import Error from "./components/Error";
 import RestaurantMenu from "./components/RestaurantMenu";
+import Shimmer from "./components/Shimmer";
+
+const Grocery = lazy(() => import("./utils/Grocery"));
 
 const AppLayout = () => {
   return (
@@ -32,6 +36,14 @@ const routerConfig = createBrowserRouter([
       {
         path: "/contact",
         element: <Contact />,
+      },
+      {
+        path: "/grocery",
+        element: (
+          <Suspense fallback={<Shimmer />}>
+            <Grocery />
+          </Suspense>
+        ),
       },
       {
         path: "/restaurants/:resId",
---
+++ utils/Grocery.js
@@ -0,0 +1,5 @@
+const Grocery = () => {
+  return <h1>Here is the big groceries list!!!!!</h1>;
+};
+
+export default Grocery;
